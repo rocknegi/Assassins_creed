7ba2ec48cd4543158cda5eea0d2585a39d33f4d8
Rohit Singh Negi <Rohit Singh Negi>
2016-07-18 13:50:02 +0530

    Initial commit A nub is born

---
+++ app/src/main/java/rocknegicorporation/shpping/MainActivity.java
@@ -0,0 +1,117 @@
+package rocknegicorporation.shpping;
+
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MENU_ITEM_LOGOUT = 1001;
+    private CoordinatorLayout coordinatorLayout;
+
+    private static String  webUrl = "https://github.com/rocknegi";
+    private static String email = "rocknegi53@gmail.com";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] addresses = {email};
+                Intent intent = new Intent (Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT,addresses);
+                intent.putExtra(Intent.EXTRA_TEXT,addresses);
+                if (intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        menu.add(0,MENU_ITEM_LOGOUT,103, R.string.logout);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+
+        switch (id){
+
+            case R.id.action_about:
+                Intent intent = new Intent(this,AboutActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_contact_me:
+                Intent intent2 = new Intent(this,contact.class);
+                startActivity(intent2);
+                 return true;
+            case MENU_ITEM_LOGOUT:
+                Snackbar.make(coordinatorLayout,"you selected logout",Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+            case R.id.action_wallpapers:
+                Snackbar.make(coordinatorLayout,"Coming soon",Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+            case R.id.action_cart:
+                Snackbar.make(coordinatorLayout,"MOTO: Nothing Is True Everything Is Permitted",Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return true;
+            case R.id.action_web:
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(webUrl));
+                if (webIntent.resolveActivity(getPackageManager())!=null){
+                    startActivity(webIntent);
+                    return true;
+                }
+            case R.id.action_settings:
+                Intent intent3 = new Intent(this,Settings_Activity.class);
+                startActivity(intent3);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/rocknegicorporation/shpping/Settings_Activity.java
@@ -0,0 +1,42 @@
+package rocknegicorporation.shpping;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Settings_Activity extends AppCompatActivity {
+    private static String email = "rocknegi53@gmail.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] addresses = {email};
+                Intent intent = new Intent (Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+                intent.putExtra(Intent.EXTRA_SUBJECT,addresses);
+                intent.putExtra(Intent.EXTRA_TEXT,addresses);
+                if (intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        });
+    }
+
+    }
+
+
