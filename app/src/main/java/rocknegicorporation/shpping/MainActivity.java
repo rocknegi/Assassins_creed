c0ec3f4a42d89db27622931ba8a435f7ea7a7f20
Rohit <rocknegi53@gmail.com>
2016-08-04 22:01:55 +0530

    added red yellow green tints changes to overflow menu used <group></group>

--- app/src/main/java/rocknegicorporation/shpping/MainActivity.java
+++ app/src/main/java/rocknegicorporation/shpping/MainActivity.java
@@ -71,11 +71,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
-        //noinspection SimplifiableIfStatement\
+
        LinearLayout content_layout = (LinearLayout) findViewById(R.id.content_layout);
 
         switch (id){
